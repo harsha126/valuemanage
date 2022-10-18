a3cf3ecdf86844897b0007eaa2412ee107186239
harsha126 <harsha.sdm111@gmail.com>
2022-10-18 12:14:54 +0530

    date format in report

--- src/main/java/com/valuemanage/services/ReportServiceImpl.java
+++ src/main/java/com/valuemanage/services/ReportServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Service
 @AllArgsConstructor
@@ -26,9 +27,9 @@ public class ReportServiceImpl implements ReportService {
     @Override
     public Report save(NewReport newReport) throws ParseException {
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
-        Comment comment = Comment.builder().text(newReport.getText()).name(newReport.getName()).date(newReport.getCommentDate()).build();
+        Comment comment = Comment.builder().text(newReport.getText()).name(newReport.getName()).date(new Date()).build();
         Comment savedComment = commentService.save(comment);
-        Report report = Report.builder().metNew(newReport.getMetNew()).metOld(newReport.getMetOld()).metTotal(newReport.getMetTotal()).orders(newReport.getOrders()).comment(savedComment).date(newReport.getReportDate()).build();
+        Report report = Report.builder().metNew(newReport.getMetNew()).metOld(newReport.getMetOld()).metTotal(newReport.getMetTotal()).orders(newReport.getOrders()).comment(savedComment).date(new Date()).build();
         report.setDate(format.parse(format.format(report.getDate())));
         return reportRepository.save(report);
     }
